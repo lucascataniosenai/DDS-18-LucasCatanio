40f253084a6103f985aa703e92926dad5a10fe0a
Lucas Catanio <lucas.catanio@aluno.senai.br>
2024-08-21 16:53:04 -0300

    Vasco da Gama

---
+++ aula9js/script.js
@@ -0,0 +1,125 @@
+//Mostra um alerta na página
+//alert("OUTRA MENSAGEM")
+
+//CONSOLE
+//console.log("Olha a mensagem de novo")
+/*
+var text = "Bolsonaro"
+var numero = 1955
+var theBestPresident = true
+var nulo = null
+var indefinido
+
+
+console.log(typeof(text))
+console.log(numero)
+console.log(theBestPresident)
+console.log(nulo)
+console.log(indefinido)
+*/
+
+
+//Operadores
+//Aritméticos: +-/*%
+
+// var a = 10
+// var b = 3
+
+// console.log("Soma:",a+b)
+// console.log("Subtração:", a-b)
+// console.log("Divisão:", a/b)
+// console.log("Multiplicação:", a*b)
+// console.log("Módulo:", a%b)
+
+// //Lógicos: &&, ||, !
+
+// var verdade = true
+// var mentira = false
+
+// console.log(verdade && mentira)
+// console.log(verdade || mentira)
+// console.log (verdade & !mentira)
+
+
+//Relacionais: >,<,>=,<=,==,!=,===,!==
+
+// var a = 10
+// var b = 5
+// var c = "10"
+// console.log("Relacionais");
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a >= b);
+// console.log(a <= b);
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a === c);
+// console.log(a !== c);
+
+//Condicionais
+//If
+
+// var estaVivo = true
+// if(estaVivo){
+//     console.log("Está vivo")
+// }
+// else if(estaVivo){
+//     console.log("Está morto");
+// }
+// else{
+//     console.log("Meia noite eu te conto");   
+// }
+
+//Switch
+// var camisa ="Cinza";
+
+// switch(camisa){
+//     case "Azul":
+//         console.log("A camisa é azul");
+//         break
+//     case "Preta":
+//         console.log("A camisa é preta");
+//         break
+//     case "Amarela":
+//         console.log("A camisa é amarela");
+//         break
+//     default:
+//         console.log("A camisa é", camisa);
+//         break       
+// }
+
+//Loops
+// for(var i = 0; i < 5; i++){
+//     console.log("oi")
+    
+// }
+// console.log("Acabou o for");
+
+// //while
+// var it = 1
+// while(it < 10){
+//     console.log("Oi");
+//     it++
+// }
+
+// function teste1(){
+//     console.log("Baiano")
+// }
+// function teste2(){
+//     console.log("Baiano2");
+    
+// }
+// teste1();
+// teste2();
+
+var  n1 = Number(prompt("Digite um algo"))
+var  n2 = Number(prompt("Digite um algo dnv"))
+soma = n1 + n2
+console.log(n1);
+console.log(n2);
+console.log("Soma:", soma);
+
+alert(n1)
+alert(n2)
+alert("Soma: " + soma)
+
